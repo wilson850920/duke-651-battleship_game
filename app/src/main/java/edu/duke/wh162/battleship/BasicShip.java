bc95af1f10d2ab1624b1e4322758f8c8e164197a
Weicheng Hung <wh162@duke.edu>
2022-01-26 23:47:27 -0500

    task6 complete

---
+++ app/src/main/java/edu/duke/wh162/battleship/BasicShip.java
@@ -0,0 +1,45 @@
+package edu.duke.wh162.battleship;
+
+public class BasicShip implements Ship<Character> {
+
+  private final Coordinate myLocation;
+
+  public BasicShip(Coordinate where) {
+    this.myLocation = where;
+  }
+  
+  @Override
+  public boolean occupiesCoordinates(Coordinate where) {
+    // TODO Auto-generated method stub
+
+    return where.equals(myLocation);
+    //return false;
+  }
+
+  @Override
+  public boolean isSunk() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void recordHitAt(Coordinate where) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public boolean wasHitAt(Coordinate where) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public Character getDisplayInfoAt(Coordinate where) {
+    // TODO Auto-generated method stub
+    return 's';
+    //return null;
+  }
+  
+
+}
