c286c73ea32c42ba9b7b7ae5f5a2ea46020dea39
Weicheng Hung <wh162@duke.edu>
2022-01-26 19:29:26 -0500

    Task4 complete

--- app/src/main/java/edu/duke/wh162/battleship/Placement.java
+++ app/src/main/java/edu/duke/wh162/battleship/Placement.java
@@ -1,15 +1,38 @@
 package edu.duke.wh162.battleship;
 
+ /**
+ * This class is the ship placement infomation with Coordinate object where and char orientation
+ * It supports two ways to construct placement: (Coordinate and char) or string
+ * It has two functions: one is to generate hash code for coordinate, one is to compare
+ * two objects are equal coordinate
+ */
+
 public class Placement {
 
+   /**
+   * The Coordinate where and orientation in coordinate.
+   */  
+  
   private final Coordinate where;
   private final char orientation;
 
+  /**
+  * @Description: Default constructor of Placement. Constructed by the passed coordinate and the passed orientation
+  * @Param:       Coordinate coordinate, char orientation
+  * @throws       IllegalArgumentException if the width or height is smaller than 0.
+  * @return:
+  */
   public Placement(Coordinate where, char orientation) {
     this.where = where;
     this.orientation = orientation;
   }
 
+  /**
+  * @Description: Constructor of Placement. Constructed by the string description like "A0V"
+  * @Param:       String descr
+  * @throws       IllegalArgumentException if the description's length is not 3 or orientation is not 'H'/'V'.
+  * @return:
+  */
   public Placement(String descr) {
 
     if (descr.length() != 3) {
@@ -27,19 +50,35 @@ public class Placement {
 
   }
   
+  /**
+   * Getter of where.
+   */
   public Coordinate getWhere() {
     return where;
   }
 
+  /**
+   * Getter of orientation.
+   */
   public char getOrient() {
     return orientation;
   }
 
+  /**
+  * @Description: Describe this Placement in String.
+  * @Param:
+  * @return:      String that describe this Placement
+  */
   @Override
   public String toString() {
     return where.toString() + ", " + orientation;
   }
 
+  /**
+  * @Description: Check if the passed object is an Object of Placement, and check if it has same value with "this".
+  * @Param:       Object o
+  * @return:      Boolean that indicate if the passed Object is equal to "this".
+  */
   @Override
   public boolean equals(Object o) {
     if (o.getClass().equals(getClass())) {
@@ -49,6 +88,11 @@ public class Placement {
     return false;
   }
 
+  /**
+  * @Description: Calculate the hashcode of this.toString()
+  * @Param:
+  * @return:      Int of hashcode
+  */
   @Override
   public int hashCode() {
     return toString().hashCode();
